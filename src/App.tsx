a1ac4a2e34e4d164dbf4543fde46c4026f5a8f28
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-08-04 11:58:36 +0400

    Made structure for some people

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import patrick from "./component/patrick";
+import Jon from "./component/Jon";
+import Jeanetta from "./component/Jeanetta";
+import Kira from "./component/Kira";
+import Daniel from "./component/Daniel";
+
 function App() {
-  return <></>;
+  const arrOfComponents = [Daniel, Jon, Jeanetta, patrick, Kira];
+
+  return (
+    <>
+      {arrOfComponents.map((Component, index) => (
+        <div key={index}>
+          <Component />
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
--- src/component/patrick.tsx
+++ src/component/patrick.tsx
@@ -1,3 +1,33 @@
 export default function patrick() {
-  return <div>patrick</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="/images/image-patrick.jpg" alt="Patricks picture" />
+        </div>
+
+        <div>
+          <span>Patrick Abrams</span>
+          <span>Verified Graduate</span>
+        </div>
+      </div>
+
+      <div>
+        <h1>
+          Awesome teaching support from TAs who did the bootcamp themselves.
+          Getting guidance from them and learning from their experiences was
+          easy.
+        </h1>
+
+        <p>
+          “ The staff seem genuinely concerned about my progress which I find
+          really refreshing. The program gave me the confidence necessary to be
+          able to go out in the world and present myself as a capable junior
+          developer. The standard is above the rest. You will get the personal
+          attention you need from an incredible community of smart and amazing
+          people. ”
+        </p>
+      </div>
+    </div>
+  );
 }
